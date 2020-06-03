33cb18371e2ecf7f61e4b7fdf99f52b44cfde930
Ryan Mackey-Paulsen <smarth55@gmail.com>
2020-06-03 09:47:00 -0400

    add log on event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ElementsExample';
+  logEvent($event) {
+    console.log($event);
+  }
 }
